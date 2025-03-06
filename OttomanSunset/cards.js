ca338798a0e1040fead3f1eaeaf2304f577177ea
sophie <mmv2005@mail.ru>
2025-03-07 00:07:47 +0300

    Most features done

--- OttomanSunset/cards.js
+++ OttomanSunset/cards.js
@@ -1,7 +1,8 @@
 class Card
 {
-    constructor(name, actions, advancing, retreating, effect) {
+    constructor(name, description, actions, advancing, retreating, effect) {
         this.name = name
+        this.description = description
         this.actions = actions
         this.advancing = advancing
         this.retreating = retreating
@@ -9,18 +10,142 @@ class Card
     }
 }
 
-export const sunriseDeck = [
-    new Card("Armenian Volunteer Units", 2, ["arab", "caucasus", "sinai"]),
-    new Card("Senussi Revolt", 2, ["mesopotamia", "galipoli"], ["sinai"]),
-    new Card("Goeben dominates Black Sea", 2, ["arab", "sinai"], ["salonika", "caucasus"]),
-    new Card("Bulgaria Joins the Central Powers", 1, ["mesopotamia", "sinai"]),
-    new Card("Enver to The Front", 2, ["caucasus", "sinai"], []), // Both offensives against Caucasus
-    new Card("Intelligence Bureau of the East", 1, ["mesopotamia"]), // Allow Intelligence
-    new Card("Galipoli Landings", 2, ["arab", "caucasus"]), // +Gallipoli, -Seddulbahir (the narrows)
-    new Card("Ghadar Conspiracy", 2, ["arab", "caucasus", "galipoli"]),
-    new Card("Gorlice-Tarnow", 3, ["caucasus", "sinai"]), // eastern theater [2]
-    new Card("Second Battle of Ypres", 1, ["caucasus", "sinai"]), // western [4]
-    new Card("Grand Duke Nicholas Takes Control", 3, ["mesopotamia", "galipoli", "salonika"]), // set Caucasus to 3
-    new Card("Turkish Minelaying", 2, ["arab", "caucasus"]), // free minefield, +1 to rolls vs Mesopotamia 
-    new Card("Jihad Declared!", 2, ["mesopotamia", "caucasus", "sinai"]) // shuffle mid-day, +1 to all rolls
+export const duskDeck = [
+    new Card("King Constantine Flees Greece", "", 3, ["caucasus", "galipoli", "salonika"], [], (gs)=>{modifierAgainst(gs, "salonika", -1)}), // -1 to salonika
+    new Card("SANDSTORMS", "", 1, ["arab"], []), // TODO die 1-3: retreat mes, 4-6: sinai, no offence vs them
+    new Card("Kaiserschlacht!", "", 4, ["caucasus", "mesopotamia"], []), // TODO kaiserchlacht enabled, +1 vs all incl. KS
+    new Card("U-Boat Campaign", "", 3, ["arab", "mesopotamia", "sinai"], [],(gs)=>{battle(gs, 'sea', 4);}), // sea [4]
+    new Card("Lawrence Stirs the Arabs", "", 1, ["caucasus"], [],(gs)=>{setStrength(gs, "arab", 3);}), // arab = 3
+    new Card("Galipoli Evacuation", "", 3, ["mesopotamia", "sinai"], []), // TODO remove galipoli, if on-map salonika=3
+    new Card("Victorio Venero", "", 3, ["arab", "galipoli", "salonika"], [],(gs)=>{battle(gs, 'east', 4);}), // eastern [4]
+    new Card("Hoffman Offensive", "", 3, ["arab", "sinai"], [],(gs)=>{modifierAgainst(gs, "caucasus")}), // +1 vs caucasus
+    new Card("Army of Islam", "", 2, ["arab", "mesopotamia", "sinai"], [],(gs)=>{modifierAll(gs)}), // +1 vs all
+    new Card("War Weariness", "", 0, [], []), // TODO war weariness (-1 to foreign)
+                                            // die roll: 1-sinai, 2-mesopotamia, 3/4-arab, 5/6-galipoli+salonika
+
+    new Card("Balfour Declaration", "", 2, ["arab", "galipoli", "mesopotamia"], [],(gs)=>{modifierAgainst(gs, "sinai")}), // +1 vs sinai
+    new Card("Allenby Takes the Helm", "", 2, ["caucasus", "arab"], [],(gs)=>{setStrength(gs, "sinai", 4);}), // sinai = 4
+    new Card("Dunsterforce", "", 2, ["arab", "sinai"], []), //  TODO skip a turn, or -1 morale
+    new Card("Bolshevik Revolution", "", 2, ["arab", "salonika", "sinai"], []), // TODO remove caucasus
 ]
+
+export const middayDeck = [
+    new Card("Yildrim", "", 1, ["caucasus", "salonika"], []), // TODO 3 tokens to stop mesopotamia/sinai
+    new Card("Verdun", "", 1, ["caucasus", "mesopotamia"], [], (gs)=>{battle(gs, 'west', 4);}), // western [4]
+    new Card("Arab Revolt","", 2, ["mesopotamia", "sinai"], [], (gs)=>{gs.showTrack("arab");}), // arab 2
+    new Card("Erzurum Offensive","", 2, ["caucasus"], [], blockCaucasus), // no caucasus for turn
+    new Card("Central Powers in Afghanistan","", 3, ["mesopotamia", "caucasus", "sinai"], [], (gs)=>{coup(gs,"afghanistan");}), // Afghan coup
+    new Card("Yudenich Named Commander in Chief","", 2, ["arab","mesopotamia"], [], (gs)=>{setStrength(gs, "caucasus", 4);}), //  caucasus = 4
+    new Card("Asia Korps","", 3, ["mesopotamia"],[]), // TODO Choose a front. +1 to all offensives there
+    new Card("Wassmuss in Persia","Переворот в Персии", 2, ["caucasus", "galipoli"],[],(gs)=>{coup(gs,"persia");}), // Persia coup
+    new Card("Landings at Salonika","", 1, ["caucasus"], [],(gs)=>{gs.showTrack("salonika");}), // salonika 2
+    new Card("Provisional Government Takes Charge","", 2, ["galipoli", "salonika", "sinai"]), // TODO caucasus -1 permanently, retreat or place
+    new Card("Brusilov Offensive","", 1, ["salonika"],[],(gs)=>{battle(gs, 'east', 3);}), // eastern [3]
+    new Card("Jutland","", 2, ["galipoli", "salonika", "mesopotamia"], [],(gs)=>{battle(gs, 'sea', 5);}), // sea [5]
+    new Card("Forcing the Narrows","", 2, ["arab", "caucasus"]), // TODO resolve FTN
+
+    new Card("Fortification of Gaza-Beersheba line","", 2, ["arab", "salonika", "mesopotamia"]), // TODO can skip a turn to build if sinai < 2
+    new Card("Mesopotamian Siege","", 1, ["caucasus", "salonika"]), // TODO +morale, mesopotamia back to 0 space, but str=4
+    new Card("Armenian Massacres","", 3, ["galipoli", "salonika", "sinai"], [], blockCaucasus), // no caucasus for turn
+    new Card("The Somme","", 1, ["galipoli", "salonika"], [], (gs)=>{battle(gs, 'west', 3);}), // western [3]
+    new Card("German U-Boats","", 2, ["caucasus", "sinai"]), // TODO No FTN, +1 vs galipoli, +2 vs salonika
+    new Card("Italy Joins the War","", 2, ["arab", "salonika"],[],(gs)=>{battle(gs, 'east', 3);}), // eastern [3]
+    new Card("Suvla Landing","", 2, ["galipoli", "caucasus", "sinai"],[],(gs)=>{setStrength(gs, "galipoli", 3);}), // if on map galipoli = 3
+
+    new Card("Sinai Pipeline","", 1, ["galipoli", "salonika"]) // TODO pipeline built, arab 2 on map, shuffle dusk
+]
+
+export const sunriseDeck = [
+    new Card("Armenian Volunteer Units", "", 2, ["arab", "caucasus", "sinai"]),
+    new Card("Senussi Revolt", "", 2, ["mesopotamia", "galipoli"], ["sinai"]),
+    new Card("Goeben dominates Black Sea","", 2, ["arab", "sinai"], ["salonika", "caucasus"]),
+    new Card("Bulgaria Joins the Central Powers","", 1, ["mesopotamia", "sinai"]),
+    new Card("Enver to The Front","Вы обязаны атаковать Кавказ", 2, ["caucasus", "sinai"], [], enver), // Both offensives against Caucasus
+    new Card("Intelligence Bureau of the East","Восточное Бюро доступно", 1, ["mesopotamia"], [], intelligence), // Allow Intelligence
+    new Card("Galipoli Landings","Галлиполи высаживается.\nСеддулбахир уничтожен", 2, ["arab", "caucasus"],[], gallipoli), // +Gallipoli, -Seddulbahir (the narrows)
+    new Card("Ghadar Conspiracy","Переворот в Индии", 2, ["arab", "caucasus", "galipoli"],[],(gs)=>{coup(gs,"india");}), // India coup
+    new Card("Gorlice-Tarnow","Битва на востоке (2)", 3, ["caucasus", "sinai"], [], (gs)=>{battle(gs, 'east', 2);}), // eastern theater [2]
+    new Card("Second Battle of Ypres","Битва на западе (4)", 1, ["caucasus", "sinai"],[], (gs)=>{battle(gs, 'west', 4);}), // western [4]
+    new Card("Grand Duke Nicholas Takes Control","Сила Кавказа=3", 3, ["mesopotamia", "galipoli", "salonika"],[], (gs)=>{setStrength(gs, "caucasus", 3);}), // set Caucasus to 3
+    new Card("Turkish Minelaying","Бесплатные Мины. +1 против Месопотамии", 2, ["arab", "caucasus"],[], (gs)=>{gs.addMine(); modifierAgainst(gs, "mesopotamia")}), // free minefield, +1 to rolls vs Mesopotamia 
+    new Card("Jihad Declared!","Замешайте Полдень. +1 ко всем броскам", 3, ["mesopotamia", "caucasus", "sinai"],[], (gs)=>{gs.shuffleIn(middayDeck); modifierAll(gs);}) // shuffle mid-day, +1 to all rolls
+]
+
+
+
+function enver(gamestate)
+{
+    gamestate.forceCaucasus = true
+}
+
+function intelligence(gamestate)
+{
+    gamestate.intelligenceAllowed = true
+}
+
+function blockCaucasus(gamestate)
+{
+    gamestate.noCaucasus = true
+}
+
+function gallipoli(gamestate)
+{
+    gamestate.showTrack("galipoli")
+    // destroy defences
+    gamestate.defences[0] = 0
+    gamestate.breakSedulbahir()
+}
+
+function coup(gamestate, name)
+{
+    gamestate.statusLabel.textContent = `Переворот. Бросьте куб.`
+    gamestate.cardBtn.disabled = true
+    gamestate.rollBtn.disabled = false
+    gamestate.rollBtn.onclick = () => {
+        if (gamestate.roll() > 5)
+            gamestate.changeMorale(+1) 
+        rollBtn.disabled = true;
+        gamestate.cardBtn.disabled = false;
+    }
+}
+
+function battle(gamestate, theater, strength)
+{
+    gamestate.statusLabel.textContent = `Битва. Бросьте куб.`
+    gamestate.cardBtn.disabled = true
+    gamestate.rollBtn.disabled = false
+    gamestate.rollBtn.onclick = () => {
+        if (gamestate.roll() + gamestate[`${theater}Res`] > strength)
+        {
+            gamestate.statusLabel.textContent = `Победа, +1 мораль`
+            gamestate.changeMorale(+1) 
+        }
+        else {
+            gamestate.statusLabel.textContent = `Поражение, -1 мораль`
+            gamestate.changeMorale(-1)
+        }
+        rollBtn.disabled = true;
+        gamestate.cardBtn.disabled = false;
+    }
+}
+
+function setStrength(gamestate, track, newStr)
+{
+    gamestate.allTracks[track].setStrength(newStr);   
+}
+
+function modifierAgainst(gamestate, track, amount)
+{
+    if (!amount) amount = 1
+    gamestate.allTracks[track].modifier = amount
+}
+
+function modifierAll(gamestate)
+{
+    gamestate.allTracks.arab.modifier=1
+    gamestate.allTracks.caucasus.modifier=1
+    gamestate.allTracks.galipoli.modifier=1
+    gamestate.allTracks.mesopotamia.modifier=1
+    gamestate.allTracks.salonika.modifier=1
+    gamestate.allTracks.sinai.modifier=1
+}
--- OttomanSunset/logic.js
+++ OttomanSunset/logic.js
@@ -1,12 +1,51 @@
 import { sunriseDeck } from "./cards.js";
 
+function random(min,max) {
+    return Math.floor((Math.random())*(max-min+1))+min;
+}
+
 const statusLabel = document.getElementById("statusLabel");
-const cardBtn = document.getElementById("cardBtn");
+const cardBtn     = document.getElementById("cardBtn");
 const rollBtn     = document.getElementById("rollBtn");
-const defenceBtn = document.getElementById("defenceBtn");
-const resWestBtn = document.getElementById("resWestBtn");
-const resSeaBtn = document.getElementById("resSeaBtn");
-const resEastBtn = document.getElementById("resEastBtn");
+const defenceBtn  = document.getElementById("defenceBtn");
+const mineBtn  = document.getElementById("mineBtn");
+const resWestBtn  = document.getElementById("resWestBtn");
+const resSeaBtn   = document.getElementById("resSeaBtn");
+const resEastBtn  = document.getElementById("resEastBtn");
+
+const moraleLabel = document.getElementById("moraleLabel")
+
+const eventCardDiv = document.getElementById("eventCard");
+
+const die = document.getElementById("die");
+
+const westResLabel = document.getElementById("west-res");
+const eastResLabel = document.getElementById("east-res");
+const seaResLabel = document.getElementById("sea-res");
+
+let resourceLeft = 6
+let morale = 0
+
+let card
+
+cardBtn.onclick = drawcard
+resWestBtn.onclick = addWestResource
+resEastBtn.onclick = addEastResource
+resSeaBtn.onclick = addSeaResource
+
+defenceBtn.onclick = () => {addDefence(3)}
+mineBtn.onclick = () => {addMine()}
+
+// 635 1073
+// 662 1239
+// 761 1097
+// 815 1254
+// 893 1096
+// 966 1222
+// 
+// 715 1171
+// 888 1169
+
 
 
 function shuffle(array) {
@@ -27,9 +66,19 @@ function disableall()
  cardBtn     .disabled= true
  rollBtn     .disabled= true
  defenceBtn  .disabled= true
+ mineBtn     .disabled= true
  resWestBtn  .disabled= true
  resSeaBtn   .disabled= true
  resEastBtn  .disabled= true
+}
+
+function enableall()
+{
+ defenceBtn  .disabled= false
+ mineBtn     .disabled= false
+ resWestBtn  .disabled= false
+ resSeaBtn   .disabled= false
+ resEastBtn  .disabled= false
 }
 
 function printMousePos(event)
@@ -56,44 +105,121 @@ class Track
         this.spaces = spaces;
         this.imgpath = "sprite/"+name+".png";
         this.currentPos = 0;
+        this.modifier = 0;
     }
 
     advance()
     {
-        this.currentPos += 1
+        if (this.name == "sinai" && this.currentPos < 2)
+            if (rollDie() >= this.strength)
+                return;
+        this.currentPos += 1;
         this.counterDiv.style.top = `${this.spaces[this.currentPos].y}px`;
         this.counterDiv.style.left = `${this.spaces[this.currentPos].x}px`;
+        
+        if (this.spaces[this.currentPos].vp)
+        {
+            changeMorale(-1)
+        }
+        if (this.currentPos >= this.spaces.length)
+        {
+            window.alert("Османская империя пала...")
+            return
+        }
+
+        
+        
     }
 
     retreat()
     {
+        if (this.currentPos == 0) return;
+        if (this.spaces[this.currentPos].vp)
+        {
+            changeMorale(+1)
+        }
         this.currentPos -= 1
         this.counterDiv.style.top = `${this.spaces[this.currentPos].y}px`;
         this.counterDiv.style.left = `${this.spaces[this.currentPos].x}px`;
     }
+
+    setStrength(newStr)
+    {
+        this.strength = newStr
+        this.counterDiv.childNodes[1].textContent = newStr
+    }
 }
 
 const allTracks = {
-    mesopotamia: new Track("mesopotamia", 4, [
+    mesopotamia: new Track("mesopotamia", 3, [
         {x:1390,y:590,vp:false},
-        {x:1360,y:452,vp:false}
+        {x:1360,y:452,vp:false},
+        {x:1235,y:428,vp:false},
+        {x:1120,y:422,vp:true},
+        {x:995,y:410,vp:true}
     ]),
     caucasus: new Track("caucasus", 2, [
         {x:1378,y:185,vp:false},
-        {x:1255,y:259,vp:false}
+        {x:1255,y:259,vp:false},
+        {x:1114,y:285,vp:false},
+        {x:977,y:269,vp:true},
+        {x:841,y:263,vp:true}
     ]),
     sinai: new Track("sinai", 3,[
         {x:500,y:910,vp:false},
-        {x:650,y:910,vp:false}
+        {x:650,y:910,vp:false},
+        {x:769,y:793,vp:false},
+        {x:843,y:667,vp:true},
+        {x:819,y:534,vp:true},
+        {x:753,y:394,vp:false}
     ]),
     galipoli: new Track("galipoli", 2,[
-        {x:442,y:673,vp:false}
+        {x:442,y:673,vp:false},
+        {x:526,y:567,vp:false},
+        {x:593,y:460,vp:true},
+        {x:636,y:351,vp:false}
+    ]),
+    salonika: new Track("salonika", 2,[
+        {x:181,y:430,vp:false},
+        {x:248,y:278,vp:false},
+        {x:381,y:181,vp:true},
+        {x:508,y:208,vp:false}
+    ]),
+    arab: new Track("arab", 2,[
+        {x:1374,y:854,vp:false},
+        {x:1240,y:697,vp:false},
+        {x:1079,y:804,vp:false},
+        {x:986,y:653,vp:false},
+        {x:919,y:534,vp:true},
+        {x:853,y:394,vp:false}
     ])
 }
 
 const gameState =
 {
-    deck: []
+    deck: [],
+    defences: [], // could add 3,3,3
+    mines: [], // 4,4
+    actionsLeft: 0,
+    westRes: 0,
+    eastRes: 0,
+    seaRes: 0,
+
+    forceCaucasus : false,
+    noCaucasus: false,
+    intelligenceAllowed : false,
+    statusLabel: statusLabel,
+    cardBtn: cardBtn,
+    rollBtn: rollBtn,
+    showTrack: showTrack,
+    roll: rollDie,
+    changeMorale:changeMorale,
+    allTracks:allTracks,
+    addMine:addMine,
+    isActionPhase: false,
+
+    shuffleIn: (newDeck) => {gameState.deck.push(...newDeck); shuffle(gameState.deck);},
+    breakSedulbahir: () => {document.getElementById("def0").textContent = "0";}
 }
 
 
@@ -105,11 +231,19 @@ function start()
    showTrack("caucasus")
    showTrack("sinai")
 
+   addDefence(4)
+   addDefence(2)
+
    gameState.deck.push(...sunriseDeck)
    shuffle(gameState.deck)
    gameloop()
 }
 
+function changeMorale(delta)
+{
+    morale += delta;
+    moraleLabel.textContent = `Мораль: ${morale}`;
+}
 
 function showTrack(name)
 {
@@ -117,6 +251,7 @@ function showTrack(name)
     counter.style.position = "absolute"
     counter.style.top = `${allTracks[name].spaces[0].y}px`
     counter.style.left = `${allTracks[name].spaces[0].x}px`
+    counter.onclick = () => { startOffensive(allTracks[name]) }
 
     let counterImg = document.createElement('img')
     counterImg.src = allTracks[name].imgpath
@@ -136,19 +271,276 @@ function showTrack(name)
 }
 
 
+
 function gameloop()
 {
-    statusLabel.textContent = "Тяните карту."
+    // reset flags
+    gameState.forceCaucasus = false;
+    gameState.noCaucasus = false;
+
+    allTracks.arab.modifier=0
+    allTracks.caucasus.modifier=0
+    allTracks.galipoli.modifier=0
+    allTracks.mesopotamia.modifier=0
+    allTracks.salonika.modifier=0
+    allTracks.sinai.modifier=0
+
+
+    statusLabel.textContent = "\tТяните карту"
     disableall()
     cardBtn.disabled = false
 }
 
 function drawcard()
 {
-    const card = gameState.deck.pop()
-    console.log(card.name)
+    
+    card = gameState.deck.pop()
+
+
+    console.log(`Drawn ${card.name}`)
+
+    // VERY ugly cleanup
+    while (eventCardDiv.childNodes[1].firstChild && eventCardDiv.childNodes[1].childNodes.length > 2) {
+        eventCardDiv.childNodes[1].removeChild(eventCardDiv.childNodes[1].lastChild);
+    }
+    while (eventCardDiv.childNodes[2].firstChild && eventCardDiv.childNodes[2].childNodes.length > 2) {
+        eventCardDiv.childNodes[2].removeChild(eventCardDiv.childNodes[2].lastChild);
+    }
+
+    // Name
+    eventCardDiv.childNodes[0].textContent = card.name
+
+    // Advancing nations
+    for (let advancingNation of card.advancing)
+    {
+        const advImg = document.createElement("img")
+        advImg.src = allTracks[advancingNation].imgpath
+        advImg.className = "halfsize"
+
+        eventCardDiv.childNodes[1].appendChild(advImg)
+
+        if (allTracks[advancingNation].counterDiv)
+            allTracks[advancingNation].advance()
+    }
+    
+    // Retreating nations
+    if (card.retreating)
+    for (let retreatingNation of card.retreating)
+    {
+        const retImg = document.createElement("img")
+        retImg.src = allTracks[retreatingNation].imgpath
+        retImg.className = "halfsize"
+
+        eventCardDiv.childNodes[2].appendChild(retImg)
+
+        if (allTracks[retreatingNation].counterDiv)
+            allTracks[retreatingNation].retreat()
+    }
+
+    // Description
+    eventCardDiv.childNodes[3].textContent = card.description;
+
+
+    // Other card stuff
+    disableall()
+
+    cardBtn.textContent = "Продолжить"
+    cardBtn.onclick = playerTurn
+    cardBtn.disabled = false
+
+    if (card.effect)
+    {
+        card.effect(gameState)
+    }
+    
 }
 
+function playerTurn()
+{
+    cardBtn.disabled = true
+    cardBtn.onclick = drawcard
+    cardBtn.textContent = "Карта"
+    gameState.actionsLeft = card.actions
+    gameState.isActionPhase = true;
+
+    if (gameState.defences.length < 6)
+        defenceBtn.disabled = false;
+    if (gameState.mines.length < 2)
+        mineBtn.disabled = false;  
+    if (card.actions > 1)
+    {
+        if (resourceLeft > 0)
+        {
+            if (gameState.westRes < 2) resWestBtn.disabled = false; 
+            if (gameState.seaRes < 2)resSeaBtn.disabled = false; 
+            if (gameState.eastRes < 2)resEastBtn.disabled = false;
+        }
+    }
+
+   
+
+    if (gameState.forceCaucasus)
+    {
+        disableall()
+        statusLabel.textContent = `Атакуйте Кавказ`
+    } else statusLabel.textContent = `\tДелайте ${card.actions} действий`
+}
+
+function spendAction()
+{
+    gameState.actionsLeft--;
+    enableall()
+    if (gameState.actionsLeft <= 0)
+    {
+        gameState.isActionPhase = false;
+        gameloop()
+        return;
+    }
+    if (gameState.actionsLeft < 2)
+    {
+        resWestBtn.disabled = true; 
+        resSeaBtn.disabled = true; 
+        resEastBtn.disabled = true;
+    }
+    if (gameState.defences.length == 6)
+        defenceBtn.disabled = true
+    if (gameState.mines.length == 2)
+        mineBtn.disabled = true
+
+    if (gameState.forceCaucasus)
+    {
+        disableall()
+        statusLabel.textContent = `Атакуйте Кавказ`
+    }
+    else statusLabel.textContent = `\tДелайте ${gameState.actionsLeft} действий`
+    
+
+    
+}
+
+function startOffensive(track)
+{
+    if (!gameState.isActionPhase)
+        return;
+    if (gameState.forceCaucasus && track.name != "caucasus")
+        return; 
+    if (gameState.noCaucasus && track.name == "caucasus")
+        return;
+    
+    
+    statusLabel.textContent = `\tКидайте куб`
+    disableall()
+    rollBtn.disabled = false
+    rollBtn.onclick = () => {doOffensive(track); rollBtn.disabled = true}
+}
+
+function doOffensive(track)
+{
+    if (rollDie() + track.modifier > track.strength)
+    {
+        track.retreat()
+    }
+    spendAction()
+}
+
+function rollDie()
+{
+    const side = random(1, 6)
+    die.src = `sprite/die/${side}.png`
+    return side
+}
+
+
+
+
+function addWestResource()
+{
+    resourceLeft--;
+    gameState.westRes++;
+    westResLabel.textContent = gameState.westRes
+    if (resourceLeft == 0 || gameState.westRes == 2) resWestBtn.disabled = true
+    spendAction()
+    spendAction()
+}
+
+function addSeaResource()
+{
+    resourceLeft--;
+    gameState.seaRes++;
+    seaResLabel.textContent = gameState.seaRes
+    if (resourceLeft == 0 || gameState.seaRes == 2) resSeaBtn.disabled = true
+    spendAction()
+    spendAction()
+}
+
+function addEastResource()
+{
+    resourceLeft--;
+    gameState.eastRes++;
+    eastResLabel.textContent = gameState.eastRes
+    if (resourceLeft == 0 || gameState.eastRes == 2) resEastBtn.disabled = true
+    spendAction()
+    spendAction()
+}
+
+const defencePos = 
+[
+    {x:635,y:1073},
+    {x:662,y:1239},
+    {x:761,y:1097},
+    {x:815,y:1254},
+    {x:893,y:1096},
+    {x:966,y:1222}
+]
+
+function addDefence(strength)
+{
+    const id = gameState.defences.length
+
+    const newDefence = document.createElement('div')
+    newDefence.style.position = 'absolute'
+    newDefence.style.top = `${defencePos[id].y}px` 
+    newDefence.style.left = `${defencePos[id].x + 20}px` 
+    newDefence.className = 'shadowText'
+    gameState.defences.push(strength)
+
+    newDefence.id = `def${id}`
+    newDefence.textContent = strength
+
+    document.body.appendChild(newDefence)
+
+    spendAction()
+}
+
+const minePos = 
+[
+    {x:715,y:1171},
+    {x:888,y:1169}
+]
+
+// 715 1171
+// 888 1169
+
+function addMine()
+{
+    const id = gameState.mines.length
+    if (id >= minePos.length)
+        return;
+
+    const newDefence = document.createElement('div')
+    newDefence.style.position = 'absolute'
+    newDefence.style.top = `${minePos[id].y}px` 
+    newDefence.style.left = `${minePos[id].x + 20}px` 
+    newDefence.className = 'shadowText'
+    
+    gameState.mines.push(4)
+
+    newDefence.textContent = 4
+
+    document.body.appendChild(newDefence)
+
+    spendAction()
+}
 
 
 start();
